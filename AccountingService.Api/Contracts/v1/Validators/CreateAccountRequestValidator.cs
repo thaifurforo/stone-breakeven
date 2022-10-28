e4aa346365c6e261dcc49adf8ac62fe74c7cd605
Thainara Furforo <thaifurforo@gmail.com>
2022-10-27 23:00:41 -0300

    Correction to Document Validation

--- AccountingService.Api/Contracts/v1/Validators/CreateAccountRequestValidator.cs
+++ AccountingService.Api/Contracts/v1/Validators/CreateAccountRequestValidator.cs
@@ -17,24 +17,26 @@ public class CreateAccountRequestValidator : AbstractValidator<CreateAccountRequ
             
             .Must(x => int.TryParse(x, out var val) && val > 0)
             .WithMessage("Invalid Agency number.");
-        
-        
+
+
         RuleFor(x => x.Document)
             .Cascade(FluentValidation.CascadeMode.Continue)
 
             .NotEmpty()
             .WithMessage("The Document is required")
 
-            .Must(x => x.IsValidCnpj())
-            .When(x => x.Document.Length == 14)
-            .WithMessage("Document: Invalid CNPJ")
+            .Length(14)
+            .When(x => x.Document.Length != 11 && x.Document.Length != 14, ApplyConditionTo.CurrentValidator)
+            .WithMessage("Document must have 11 (CPF) or 14 (CNPJ) characters")
 
             .Must(x => x.IsValidCpf())
-            .When(x => x.Document.Length == 11)
+            .When(x => x.Document.Length == 11, ApplyConditionTo.CurrentValidator)
             .WithMessage("Document: Invalid CPF")
 
-            .Length(14)
-            .When(x => x.Document.Length != 11 && x.Document.Length != 14)
-            .WithMessage("Document must have 11 (CPF) or 14 (CNPJ) characters");
+            .Must(x => x.IsValidCnpj())
+            .When(x => x.Document.Length == 14, ApplyConditionTo.CurrentValidator)
+            .WithMessage("Document: Invalid CNPJ");
+
+
     }
 }
