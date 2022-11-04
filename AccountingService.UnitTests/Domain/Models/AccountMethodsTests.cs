526ac3d2185c3cbd7e831d686e9bffbbff031589
Thainara Furforo <thaifurforo@gmail.com>
2022-11-04 15:00:25 -0300

    Created new AccountMethodsTests

--- AccountingService.UnitTests/Domain/Models/AccountMethodsTests.cs
+++ AccountingService.UnitTests/Domain/Models/AccountMethodsTests.cs
@@ -1,5 +1,6 @@
 using AccountingService.Domain.Models;
 using AutoFixture;
+using FluentAssertions;
 
 namespace AccountingService.UnitTests.Domain.Models;
 
@@ -10,11 +11,11 @@ public class AccountMethodsTests
     
     public AccountMethodsTests()
     {
-        _fixture.Register(() => default(DateOnly));
+        _fixture.Register(() => default(DateOnly)); 
     }
     
     [Fact]
-    public void FindAccountById_GivenAnIdNumber_ShouldAccountNumberBeCorrectlyGenerated()
+    public void CreateAccount_GivenAnyIdNumber_ShouldAccountNumberBeCorrectlyGenerated()
     {
         var account = _fixture.Build<Account>()
             .Create();
@@ -22,5 +23,24 @@ public class AccountMethodsTests
         string accountNumber = account.GetAccountNumber(account.Id);
 
         Assert.Equal(accountNumber, account.Number);
+    }
+
+    [Theory]
+    [InlineData(1, "000001-2")]
+    [InlineData(24, "000024-3")]
+    [InlineData(897, "000897-7")]
+    [InlineData(3674, "003674-2")]
+    [InlineData(98741, "098741-4")]
+    [InlineData(542198, "542198-6")]
+    public void CreateAccount_GivenAnIdNumber_ShouldAccountNumberBeCorrectlyGenerated(int id, string expectedAccountNumber)
+    {
+        
+        var account = _fixture.Build<Account>()
+            .With(x => x.Id, id)
+            .Create();
+
+        account.Number = account.GetAccountNumber(id);
+        
+        account.Number.Should().Be(expectedAccountNumber);
     }
 }
