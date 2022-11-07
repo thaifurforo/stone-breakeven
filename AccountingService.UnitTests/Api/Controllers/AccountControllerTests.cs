56872aaae108416d773afcf320ebe56639ad2654
Thainara Furforo <thaifurforo@gmail.com>
2022-11-07 14:52:48 -0300

    Implemented DeactivateAccount Controller tests

--- AccountingService.UnitTests/Api/Controllers/AccountControllerTests.cs
+++ AccountingService.UnitTests/Api/Controllers/AccountControllerTests.cs
@@ -1,4 +1,5 @@
 using AccountingService.Api.Contracts.v1.Requests;
+using AccountingService.Api.Contracts.v1.Response;
 using AccountingService.Api.Controllers;
 using AccountingService.Domain.Models;
 using AutoFixture;
@@ -12,8 +13,9 @@ public class AccountControllerTests
     private readonly Fixture _fixture = new();
     private readonly AccountController _controller = new();
 
-    private const string agency = "001";
-    private const string document = "12345678909";
+    private const string Agency = "001";
+    private const string Document = "12345678909";
+    private const decimal Balance = 1;
     
     public AccountControllerTests()
     {
@@ -33,13 +35,39 @@ public class AccountControllerTests
     public async Task CreateAccount_GivenValidRequest_ShouldReturnAccepted()
     {
         var request = _fixture.Build<CreateAccountRequest>()
-            .With(x => x.Agency, agency)
-            .With(x => x.Document, document)
+            .With(x => x.Agency, Agency)
+            .With(x => x.Document, Document)
             .Create();
         var response = await _controller.CreateAccount(request);
 
         response.Should().BeOfType<OkObjectResult>();
     }
     
+     [Fact]
+     public async Task DeactivateAccount_GivenValidRequest_ShouldReturnAccepted()
+     {
+         var account = _fixture.Build<Account>()
+             .Create();
+        
+         AccountList.Accounts.Add(account);
+         
+         var response = await _controller.DeactivateAccount(account.Id);
+
+         response.Should().BeOfType<BadRequestObjectResult>();
+     }
+     
+     [Fact]
+     public async Task DeactivateAccount_GivenInvalidRequest_ShouldReturnAccepted()
+     {
+         var account = _fixture.Build<Account>()
+             .With(x => x.Balance, Balance)
+             .Create();
+        
+         AccountList.Accounts.Add(account);
+         
+         var response = await _controller.DeactivateAccount(account.Id);
+
+         response.Should().BeOfType<OkObjectResult>();
+     }
 }
 
