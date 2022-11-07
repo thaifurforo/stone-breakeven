6069c97fe5f2f5ca39f424fb134ff9abd1601fc4
Thainara Furforo <thaifurforo@gmail.com>
2022-11-07 14:40:38 -0300

    Implemented Deactivate Account Request Validator Tests

--- AccountingService.UnitTests/Api/Contracts/v1/Validators/DeactivateAccountRequestValidatorTests.cs
+++ AccountingService.UnitTests/Api/Contracts/v1/Validators/DeactivateAccountRequestValidatorTests.cs
@@ -1,25 +1,42 @@
 using AccountingService.Api.Contracts.v1.Requests;
+using AccountingService.Api.Contracts.v1.Response;
 using AccountingService.Api.Contracts.v1.Validators;
+using AccountingService.Domain.Models;
+using AutoFixture;
 using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AccountingService.UnitTests.Api.Contracts.v1.Validators;
 
 public class DeactivateAccountRequestValidatorTests
 {
     private readonly DeactivateAccountRequestValidator _validator = new();
+    private readonly Fixture _fixture = new();
+    
+    public DeactivateAccountRequestValidatorTests()
+    {
+        _fixture.Register(() => default(DateOnly));
+    }
     
     public static readonly object[][] TheoryData =
     {
-        new object[] { new DateOnly(2022, 11, 4), new DateOnly(2022, 11, 5), true},
-        new object[] { new DateOnly(2022, 11, 4), new DateOnly(2022, 11, 3), false}
+        new object[] { new DateOnly(2022, 11, 4), new DateOnly(2022, 11, 5), 0, true},
+        new object[] { new DateOnly(2022, 11, 4), new DateOnly(2022, 11, 3), 0, false},
+        new object[] { new DateOnly(2022, 11, 4), new DateOnly(2022, 11, 5), 1, false},
+        new object[] { new DateOnly(2022, 11, 4), new DateOnly(2022, 11, 5), -1, false},
     };
 
     [Theory, MemberData(nameof(TheoryData))]
 
-    public void ValidateDeactivateAccountRequest_GivenARequest_MusBeValidatedLikeExpected(DateOnly openingDate,
-        DateOnly closingDate, bool expected)
+    public void ValidateDeactivateAccountRequest_GivenARequest_MustBeValidatedLikeExpected(DateOnly openingDate,
+        DateOnly closingDate, decimal balance, bool expected)
     {
-        var mockRequest = new DeactivateAccountRequest() {OpeningDate = openingDate, ClosingDate = closingDate};
+        Account account = _fixture.Build<Account>()
+            .Create();
+        
+        AccountList.Accounts.Add(account);
+        
+        var mockRequest = new DeactivateAccountRequest(account.Id) {OpeningDate = openingDate, ClosingDate = closingDate, Balance = balance};
 
         var result = _validator.Validate(mockRequest);
 
