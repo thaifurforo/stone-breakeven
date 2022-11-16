2809538176f78a204146964c06e70712cb527377
Thainara Furforo <thaifurforo@gmail.com>
2022-11-16 14:14:57 -0300

    Functional unit tests for Account Controller

--- AccountingService.UnitTests/Api/Controllers/AccountControllerTests.cs
+++ AccountingService.UnitTests/Api/Controllers/AccountControllerTests.cs
@@ -1,31 +1,51 @@
+using AccountingService.Api.Contracts.v1.Requests;
 using AccountingService.Api.Controllers;
 using AccountingService.Domain.Commands;
 using AccountingService.Domain.Contracts;
+using AccountingService.Domain.Models;
 using AccountingService.Repository;
 using AccountingService.Repository.Repositories;
+using FluentAssertions;
 using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 
 namespace AccountingService.UnitTests.Api.Controllers;
 
-public class AccountControllerTests
+public class AccountControllerTests : IAsyncLifetime
 {
     private readonly AccountController _accountController;
-    private readonly ReadModelInMemoryContext _readModelInMemoryContext;
     private readonly IAccountRepository _accountRepository;
     private readonly Mock<IMediator> _mediator = new();
 
+    private readonly Account _account;
+    
+    private const string GenericAgency = "001";
+    private const string GenericDocument = "12345678909";
+    
 
     public AccountControllerTests()
     {
-        var options = new DbContextOptionsBuilder<ReadModelInMemoryContext>()
+        var options = new DbContextOptionsBuilder<ReadModelSqlContext>()
             .UseInMemoryDatabase(databaseName: "FakeDatabase")
             .Options;
-        _readModelInMemoryContext = new ReadModelInMemoryContext(options);
-        _accountRepository = new AccountInMemoryRepository(_readModelInMemoryContext);
-
+        var readModelSqlContext = new ReadModelSqlContext(options);
+        _accountRepository = new AccountSqlRepository(readModelSqlContext);
         _accountController = new AccountController(_accountRepository, _mediator.Object);
+        
+        _account = new Account(GenericDocument, GenericAgency);
+    }
+    
+    public async Task InitializeAsync()
+    {
+        await _accountRepository.AddAccount(_account);
+        await _accountRepository.Save();
+    }
+
+    public Task DisposeAsync()
+    {
+        return Task.CompletedTask;
     }
 
     [Fact]
@@ -33,15 +53,33 @@ public class AccountControllerTests
     {
         // _mediator.Setup(x => x.Send(It.IsAny<IRequest<Unit>>(), It.IsAny<CancellationToken>()));
         await _accountController.CreateAccount(It.IsAny<CreateAccountCommand>());
-        _mediator.Verify(x => x.Send(It.IsAny<CreateAccountCommand>(), It.IsAny<CancellationToken>()), Times.Once);
+        _mediator.Verify(x => x.Send(It.IsAny<CreateAccountCommand>(),
+            It.IsAny<CancellationToken>()), Times.Once);
     }
-    
+
     [Fact]
     public async void DeactivateAccountTest()
     {
-        await _accountController.DeactivateAccount(It.IsAny<int>());
-        _mediator.Verify(x => x.Send(It.IsAny<DeactivateAccountCommand>(), It.IsAny<CancellationToken>()), Times.Once);
-    }
-    
-}
+        var result = await _accountController.DeactivateAccount(new GetByAccountId() { Id = _account.Id });
 
+        _mediator.Verify(x => x.Send(It.IsAny<DeactivateAccountCommand>(),
+            It.IsAny<CancellationToken>()), Times.Once);
+
+        result.Should().BeOfType<OkObjectResult>();
+    }
+
+    [Fact]
+    public async void GetAllAccountsTest()
+    {
+        var result = await _accountController.GetAllAccounts();
+        result.Should().BeOfType<OkObjectResult>();
+    }
+
+    [Fact]
+    public async void GetAccountTest()
+    {
+        var request = new GetByAccountId() { Id = _account.Id };
+        var result = await _accountController.GetAccount(request);
+        result.Should().BeOfType<OkObjectResult>();
+    }
+}
