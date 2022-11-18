1b697f09fd651478d838c3e22fe6f454445e756e
Thainara Furforo <thaifurforo@gmail.com>
2022-11-18 19:45:35 -0300

    Remove not used property

--- AccountingService.Domain/Commands/DeactivateAccountCommand.cs
+++ AccountingService.Domain/Commands/DeactivateAccountCommand.cs
@@ -5,5 +5,4 @@ namespace AccountingService.Domain.Commands;
 public class DeactivateAccountCommand : IRequest<object>
 {
     public int Id { get; set; }
-    public DateTime ClosingDate { get; set; }
 }
